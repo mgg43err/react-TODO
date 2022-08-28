3cd09be567457c99e3fa82812e138bc80ea0a626
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2022-08-28 20:07:50 +0300

    first commit

---
+++ src/ToDoForm.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+
+
+function ToDoForm({addTask}) {
+  const [userInput, setUserInput] = useState("")
+
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addTask(userInput)
+    setUserInput("")
+  }
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") { 
+      handleSubmit(e)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={userInput}
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        placeholder="write your task"
+      />
+      <button>
+        Save
+      </button>
+    </form>
+  )
+}
+
+export default ToDoForm
